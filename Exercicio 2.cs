eca7606fc917b2e4256698bdc7d0e1a8369af8f5
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-12 21:38:11 -0300

    Exercicio 2

---
+++ Exercicio 2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //exercícios 2
+            //Nome:Lucas Luna de Queiroz; Matricula: 8139081; Turma:01AM
+            double[] v = new double[10];
+            double media=0, am=0;
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.WriteLine("Digite sua nota");
+                v[i] = double.Parse(Console.ReadLine());
+                media += v[i];
+            }
+            media /= 10;
+            Console.WriteLine(media);
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (v[i] > media)
+                {
+                    am++;
+                }
+            }
+            Console.WriteLine(am);
+           
+        }
+    }
+}
