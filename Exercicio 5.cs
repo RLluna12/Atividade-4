ea8ff6b3070b593deb03779090f37a1cfdec3b47
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-14 17:05:30 -0300

    Exercicio 5

---
+++ Exercicio 5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp57
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //exercícios 5
+            //Nome:Lucas Luna de Queiroz; Matricula: 8139081; Turma:01AM
+            double[] a = new double[10];
+            double x;
+            double[] m = new double[10];
+            Console.WriteLine("Digite o valor de X: ");
+            x = double.Parse(Console.ReadLine());   
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine("Digite o numero " + (i+1) + ":");
+                a[i] = double.Parse(Console.ReadLine());
+            }
+            for(int i = 0; i < m.Length; i++)
+            {
+                m[i] = a[i]*x;
+                Console.WriteLine(m[i]);
+            }
+
+
+        }
+    }
+}
