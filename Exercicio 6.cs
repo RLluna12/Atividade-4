1b3ed311d70520b8fe6a0f35e33cd54d7a0ff330
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2022-10-14 22:16:15 -0300

    Exercicio 6

---
+++ Exercicio 6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp59
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //exercícios 6
+            //Nome:Lucas Luna de Queiroz; Matricula: 8139081; Turma:01AM
+            int[] v = new int[10];
+            for (int i = 0; i < v.Length; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Inverso do q foi digitado: ");
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.WriteLine(v[9-i]);
+            }
+        }
+    }
+}
